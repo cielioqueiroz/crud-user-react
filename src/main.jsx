01ae3aa64934af8ea822df407c40229d169bd78d
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-01-06 16:37:46 -0300

    configurando Material UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routers/index";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme/defaults";
+import "./assets/index.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
