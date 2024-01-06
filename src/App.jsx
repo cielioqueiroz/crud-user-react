9e9665930e29bd9a46f5b953ef1f456b3bf1f044
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-01-06 17:09:58 -0300

    criando estrutura padrão

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+import { Box, Drawer } from "@mui/material";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
-  return <Outlet />;
+  const theme = useTheme();
+  return (
+    <>
+      <Drawer variant="permanent" anchor="left">
+        teste
+        <Link to="/home">Home</Link>
+        <Link to="/users">Users</Link>
+      </Drawer>
+      <Box marginLeft={theme.spacing(28)}>
+        <header>Teste Header</header>
+        <Outlet />
+      </Box>
+    </>
+  );
 }
 
 export default App;
