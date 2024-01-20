0b742cbeee461879c8f5d542d4feed1a9e73f00b
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-01-20 12:06:20 -0300

    configurando espacamento do drawer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import { useTheme } from "@mui/material/styles";
 import { Box, Drawer } from "@mui/material";
 import { Link, Outlet } from "react-router-dom";
 
+const drawerWidth = 224;
+
 function App() {
   const theme = useTheme();
   return (
     <>
-      <Drawer variant="permanent" anchor="left">
-        teste
+      <Drawer
+        variant="permanent"
+        anchor="left"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
         <Link to="/home">Home</Link>
         <Link to="/users">Users</Link>
       </Drawer>
