338bd4494ce04c1bedaf695da08fe320a6026409
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-01-25 21:19:10 -0300

    adicioiando cor na paleta

--- src/theme/defaults.js
+++ src/theme/defaults.js
@@ -1,7 +1,12 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
-  typography: { fontFamily: "Roboto, sans-serif" },
+  typography: { 
+    fontFamily: "Roboto, sans-serif",
+    myCustomText:{
+      color: "black",
+    } 
+  },
 });
 
 export default theme;
