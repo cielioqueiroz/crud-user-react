1b84224c1ac383f2101e3fd932534827c6a1fee6
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-02-24 12:35:01 -0300

    criando setLoading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,23 +12,28 @@ import {
 import PageHeader from "../components/PageHeader";
 import supabase from "./../api/supabase";
 import formater from "../helpers/global";
+import Loading from "../components/Loading";
 
 function Home() {
   const [usuario, setUsuario] = useState(0);
   const [status, setStatus] = useState(0);
   const [salario, setSalario] = useState(0);
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getUsers();
   }, []);
 
   async function getUsers() {
+    setLoading(true);
     const { data } = await supabase.from("usuarios").select("*");
+
     setUsers(data);
     countUsers(data.length);
     countActiveUsers(data);
     countSalario(data);
+    setLoading(false);
   }
 
   function countUsers(total) {
@@ -65,56 +70,71 @@ function Home() {
           height: "75%",
         }}
       >
-        <div>
-          <div>
-            <p>
-              Total de Usuários: <span>{usuario}</span>
-            </p>
-          </div>
-          <div>
-            <p>
-              Total de Salários: <span>{salario}</span>
-            </p>
-          </div>
-          <div>
-            <p>
-              Total de Usuários Ativos: <span>{status}</span>
-            </p>
-          </div>
-        </div>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Nome</TableCell>
-                <TableCell align="left">Salário</TableCell>
-                <TableCell align="left">Função</TableCell>
-                <TableCell align="left">E-mail</TableCell>
-                <TableCell align="left">Contato</TableCell>
-                <TableCell align="left">Status</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {users.map((usuario) => (
-                <TableRow
-                  key={usuario.id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {usuario.name}
-                  </TableCell>
-                  <TableCell align="left">
-                    {formater(usuario.salario)}
-                  </TableCell>
-                  <TableCell align="left">{usuario.funcao}</TableCell>
-                  <TableCell align="left">{usuario.email}</TableCell>
-                  <TableCell align="left">{usuario.contato}</TableCell>
-                  <TableCell align="left">{usuario.status}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        {loading ? (
+          <Box
+            sx={{
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Loading />
+          </Box>
+        ) : (
+          <>
+            <div>
+              <div>
+                <p>
+                  Total de Usuários: <span>{usuario}</span>
+                </p>
+              </div>
+              <div>
+                <p>
+                  Total de Salários: <span>{salario}</span>
+                </p>
+              </div>
+              <div>
+                <p>
+                  Total de Usuários Ativos: <span>{status}</span>
+                </p>
+              </div>
+            </div>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Nome</TableCell>
+                    <TableCell align="left">Salário</TableCell>
+                    <TableCell align="left">Função</TableCell>
+                    <TableCell align="left">E-mail</TableCell>
+                    <TableCell align="left">Contato</TableCell>
+                    <TableCell align="left">Status</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {users.map((usuario) => (
+                    <TableRow
+                      key={usuario.id}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {usuario.name}
+                      </TableCell>
+                      <TableCell align="left">
+                        {formater(usuario.salario)}
+                      </TableCell>
+                      <TableCell align="left">{usuario.funcao}</TableCell>
+                      <TableCell align="left">{usuario.email}</TableCell>
+                      <TableCell align="left">{usuario.contato}</TableCell>
+                      <TableCell align="left">{usuario.status}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </>
+        )}
       </div>
     </Box>
   );
