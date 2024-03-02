bf3a233b6be398f7ca76516660dfc447de693ad7
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-03-02 17:19:00 -0300

    adicionando props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import { Button } from "@mui/material";
 import React from "react";
 
-export default function ActionButton({ action, children }) {
+export default function ActionButton({ action = null, children, type = "button" }) {
   return (
-    <Button variant="contained" onClick={action}>
+    <Button variant="contained" type={type} onClick={action}>
       {children}
     </Button>
   );
