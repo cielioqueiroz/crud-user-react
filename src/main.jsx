cdfce2e499895d4edb8c6288e69536471ef4f8da
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-03-19 17:00:07 -0300

    organizando arquivos

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./Routers/index";
 import { ThemeProvider } from "@emotion/react";
+import { UserProvider } from "./context/useUserContext";
+ 
+import router from "./Routers/index";
 import theme from "./theme/defaults";
-import { UserProvider } from "./context/useUserContext"; 
+
 import "./assets/index.css";
 
 
